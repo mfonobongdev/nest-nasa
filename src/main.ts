2a42d00a880ae6c6abcdb41294faa3bd7f7dff2a
mfonobongdev <93042878+mfonobongdev@users.noreply.github.com>
2024-01-10 13:54:19 +0100

    added route logging

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { ZodExceptionFilter } from './utils/zod/zod-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new ZodExceptionFilter());
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
   await app.listen(3333);
 }
 bootstrap();
